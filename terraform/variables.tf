e792f5fe9b009b4fecfb40afdcfb45cdf743c3e9
unknown <fritzalbrecht2@gmail.com>
2025-03-03 16:56:42 -0500

    fix:cloudflare refs

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -95,3 +95,24 @@ variable "ecs_image_url" {
   type        = string
   default = "144131464452.dkr.ecr.us-east-1.amazonaws.com/flask-docker-demo-app:latest"
 }
+
+variable "cloudflare_zone_id" {
+  description = "Cloudfare DNS zone ID"
+  type        = string
+}
+
+variable "cloudflare_account_id" {
+  description = "Cloudflare account ID"
+  type        = string
+}
+
+variable "cloudflare_api_token" {
+  description = "The Cloudflare API Token for authentication"
+  type        = string
+}
+
+variable "cloudflare_email" {
+  description = "The Cloudflare email for authentication"
+  type        = string
+  default     = "fritzalbrecht2@gmail.com"
+}
