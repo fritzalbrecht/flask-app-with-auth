0d3e3ad28f4c5d0a07804c6508b4cbabea07f01b
fritzalbrecht <fritzalbrecht2@gmail.com>
2025-04-02 08:44:27 -0400

    fix: very expensive NAT gateways

--- terraform/main.tf
+++ terraform/main.tf
@@ -152,50 +152,6 @@ resource "aws_security_group" "ecs_service_sg" {
 }
 
 
-# Create the NACL rules for the public NACL.
-resource "aws_network_acl_rule" "public_ingress" {
-  network_acl_id = aws_network_acl.public.id
-  rule_number    = 100
-  protocol       = "-1"
-  rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
-}
-
-resource "aws_network_acl_rule" "public_egress" {
-  network_acl_id = aws_network_acl.public.id
-  rule_number    = 100
-  egress         = true
-  protocol       = "-1"
-  rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
-
-}
-
-# Create a private NACL.
-resource "aws_network_acl" "private" {
-  vpc_id = aws_vpc.main.id
-}
-
-
-# Create the NACL rules for the private NACL.
-resource "aws_network_acl_rule" "private_ingress" {
-  network_acl_id = aws_network_acl.private.id
-  rule_number    = 100
-  protocol       = "-1"
-  rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
-}
-
-resource "aws_network_acl_rule" "private_egress" {
-  network_acl_id = aws_network_acl.private.id
-  rule_number    = 100
-  egress         = true
-  protocol       = "-1"
-  rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
-
-}
-
 # Create a CloudWatch log group.
 resource "aws_cloudwatch_log_group" "ecs_logs" {
   name              = "ecs-logs"
@@ -207,7 +163,6 @@ resource "aws_lb" "app_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
 }
 
 resource "aws_lb_target_group" "app_tg" {
